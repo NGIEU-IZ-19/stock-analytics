aec652e3831d1e007685b4d69b75bf0063dd178c
Ilya Shatov <4i9wh9hrw@mozmail.com>
2022-08-23 00:20:01 +0300

    Add get volatility, get volume, get earnings

--- stock_analytics/base.py
+++ stock_analytics/base.py
@@ -18,23 +18,23 @@ from datetime import datetime as dt
 
 import pandas as pd
 import yfinance as yf
+import numpy
 
 
-class stock_technical_analytics:
+
+class stock_analytics:
 
     def __init__(self, instrument, year=dt.utcnow().strftime("%Y"), month=dt.utcnow().strftime("%m"),
                  day=dt.utcnow().strftime("%d"), timedelta=1, fullPeriod=False, period='5y', interval='1d'):
         """
         A class that uses the yfinance api and lets you plot indicators of a stock such as:
-
-
             - History data
             - Simple moving averages (SMA)
             - Volatility
             - Volumes
         """
 
-        # self.df_earnings = self.get_earnings
+        self.df_earnings = self.get_earnings
         self.instrument = instrument
         self.year = int(year)
         self.month = int(month)
@@ -59,11 +59,7 @@ class stock_technical_analytics:
             df = df.history(period=self.period, interval=self.interval)
         else:
             df = df.history(interval='1m', start=str(self.start), end=str(self.end))
-
-        stock_history_df = pd.DataFrame(
-            {'Open': df['Open'], 'High': df['High'], 'Low': df['Low'], 'Close': df['Close'], 'Volume': df['Volume']})
-
-        return stock_history_df
+        return df
 
     def get_sma_50(self, period=50):
         """
@@ -89,34 +85,43 @@ class stock_technical_analytics:
 
         return self.df_history['SMA200']
 
-    # def volatility(self, period=200):
-    #     """
-    #     Volatility is a measurement of the variation of prices over time.
-    #
-    #     Method returns a dataframe with 50 days average value of money
-    #     """
-    #     self.df_history['volatility'] = numpy.log(self.df_history['Close']/self.df_history['Close'].shift())
-    #     # self.df_history['volatility'].std()
-    #     volatility = self.df_history['volatility'].std()*252**.5
-    #
-    #     # self.df_history['SMA200'] = self.df_history['Close'].rolling(window=period).mean()
-    #
-    #     print(volatility)
+    def volatility(self):
+        """
+        Volatility is a measurement of the variation of prices over time.
+    
+        Method returns:
+            -   Volatility (annualized standard deviation) numerical value, in percent
+            -   Daily log return of the Close price
+        """
+        self.df_history['Log returns'] = numpy.log(self.df_history['Close']/self.df_history['Close'].shift())
+        volatility_year = self.df_history['Log returns'].std()*252**.5
+        volatility_percent = str(round(volatility_year, 4) * 100)
 
+        return volatility_year, volatility_percent, self.df_history['Log returns']
 
-    # def get_earnings(self):
-    #     """
-    #     Earnings are perhaps the single most important and most closely studied number in a company's financial
-    #     statements. It shows a company's real profitability compared to the analyst estimates, its own historical
-    #     performance, and the earnings of its competitors and industry peers.
-    #
-    #     Method returns a dataframe with Earnings data.
-    #     """
-    #
-    #     df = yf.Ticker(self.instrument)
-    #     self.df_earnings = df.earnings
-    #
-    #     print(self.df_earnings)
+    def volume(self):
+        """
+        # Volatility is a measurement of the variation of prices over time.
+        #
+        # Method returns:
+        #     -   Volatility (annualized standard deviation) numerical value, in percent
+        #     -   Daily log return of the Close price
+        """
+        return self.df_history['Volume']
+
+    def get_earnings(self):
+        """
+        Earnings are perhaps the single most important and most closely studied number in a company's financial
+        statements. It shows a company's real profitability compared to the analyst estimates, its own historical
+        performance, and the earnings of its competitors and industry peers.
+
+        Method returns a dataframe with Earnings data.
+        """
+
+        df = yf.Ticker(self.instrument)
+        self.df_earnings = df.earnings
+
+        return self.df_self.df_earnings
 
     # def obv(self):
     #     """
@@ -142,11 +147,10 @@ class stock_technical_analytics:
     #     print(self.df_earnings)
 
 
-# if __name__ == '__main__':
-#     #     #Example of plotting the Apple stock
-#     # admin = stock_technical_analytics("AAPL", fullPeriod=True, period='1y', interval='1d')
-#     # sma = pd.DataFrame(admin.get_sma_50())
-#     # print(sma)
-#     # admin.get_SMA_200()
-#     # admin.volatility()
-#     # print(dataframe)
+if __name__ == '__main__':
+    #     #Example of plotting the Apple stock
+    admin = stock_analytics("AAPL", fullPeriod=True, period='1y', interval='1d')
+    admin_print = pd.DataFrame(admin.df_history)
+    print(admin_print.columns)
+    print(admin.get_earnings())
+
